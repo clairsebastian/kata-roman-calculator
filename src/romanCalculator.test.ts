4ea6e817838fbdaf5e58df7f333ff74a35fb0a1f
Clair Mary Sebastian <clairs@sahaj.ai>
2023-03-01 18:50:13 +1100

    added check for invalid roman numeral

--- src/romanCalculator.test.ts
+++ src/romanCalculator.test.ts
@@ -6,4 +6,18 @@ test('add sums the roman numerals', ()=>{
     assert.equal(RomanCalculator.add("I", "II"), "III")
     assert.equal(RomanCalculator.add("X", "X"), "XX")
     assert.equal(RomanCalculator.add("C", "C"), "CC")
+    assert.equal(RomanCalculator.add("V", "I"), "VI")
+    assert.equal(RomanCalculator.add("X", "II"), "XII")
+})
+
+test('add returns INVALID INPUT given repeated V, L or D in a numeral', ()=>{    
+    assert.equal(RomanCalculator.add("VV", "I"), "INVALID INPUT")
+    assert.equal(RomanCalculator.add("I", "LL"), "INVALID INPUT")
+    assert.equal(RomanCalculator.add("X", "DD"), "INVALID INPUT")
+})
+
+test('add returns INVALID INPUT given a non roman numeral', ()=>{    
+    assert.equal(RomanCalculator.add("VV", "3"), "INVALID INPUT")
+    assert.equal(RomanCalculator.add("y", "LL"), "INVALID INPUT")
+    assert.equal(RomanCalculator.add("X", "d"), "INVALID INPUT")
 })
--- src/romanCalculator.ts
+++ src/romanCalculator.ts
@@ -1,7 +1,42 @@
 export class RomanCalculator{
 
     static add(numeralOne:string, numeralTwo:string):string{
+        if (!RomanCalculator.isValid(numeralOne) || !RomanCalculator.isValid(numeralTwo)){
+            return "INVALID INPUT"
+        }
+
         return numeralOne + numeralTwo;
     }
+    static isValid(numeral:string):boolean{
 
+        for (let index = 0; index < numeral.length; index++) {
+            if (numeralsRuleMap.has(numeral[index])) {
+                const allowedRepetitions = numeralsRuleMap.get(numeral[index])?.allowedRepetitions as number;
+                const invalidRepetition = numeral[index].repeat(allowedRepetitions + 1)
+                console.log(`${numeral[index]} is allowed ${allowedRepetitions} times. invalidRepetition = ${invalidRepetition}`);
+                if (numeral.includes(invalidRepetition)) {
+                    return false;
+                }
+            }
+            else { 
+                return false;
+            }             
+        }
+
+        return true;
+    }
+}
+
+const numeralsRuleMap = new Map<string, NumeralRule>([
+    ["I", { allowedRepetitions: 3 }],
+    ["V", { allowedRepetitions: 1 }],
+    ["X", { allowedRepetitions: 3 }],
+    ["L", { allowedRepetitions: 1 }],
+    ["C", { allowedRepetitions: 3 }],
+    ["D", { allowedRepetitions: 1 }],
+    ["M", { allowedRepetitions: 3 }]
+]);
+
+type NumeralRule = {
+    allowedRepetitions:number 
 }
