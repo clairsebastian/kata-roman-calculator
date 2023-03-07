14bf4ac746774f415c0842e73d6aebb41f6a2ed9
Clair Mary Sebastian <clairs@sahaj.ai>
2023-03-07 11:56:17 +1100

    RomanCalculator rules implemented using regex. Added behaviour for toNumeral and toNumber

--- src/romanCalculator.test.ts
+++ src/romanCalculator.test.ts
@@ -1,33 +1,122 @@
 import {test,assert} from 'vitest';
 import { RomanCalculator } from './romanCalculator';
 
-test('add sums the roman numerals', ()=>{    
-    assert.equal(RomanCalculator.add("I", "I"), "II")
-    assert.equal(RomanCalculator.add("I", "II"), "III")
-    assert.equal(RomanCalculator.add("X", "X"), "XX")
-    assert.equal(RomanCalculator.add("C", "C"), "CC")
-    assert.equal(RomanCalculator.add("V", "I"), "VI")
-    assert.equal(RomanCalculator.add("X", "II"), "XII")
-    assert.equal(RomanCalculator.add("X", "VI"), "XVI")
-    assert.equal(RomanCalculator.add("L", "XV"), "LXV")
-    assert.equal(RomanCalculator.add("X", "XX"), "XXX")
+
+test('isValid returns true given valid roman numeral', () => {    
+    const calc = new RomanCalculator();
+
+    assert.isTrue(calc.isValid("I"))
+    assert.isTrue(calc.isValid("II"))
+    assert.isTrue(calc.isValid("III"))    
+    assert.isTrue(calc.isValid("IV"))
+    assert.isTrue(calc.isValid("V"))
+    assert.isTrue(calc.isValid("VI"))
+    assert.isTrue(calc.isValid("VII"))
+    assert.isTrue(calc.isValid("VIII"))
+    assert.isTrue(calc.isValid("IX"))
+    assert.isTrue(calc.isValid("XXX"))
+    assert.isTrue(calc.isValid("XXXIX"))
+    assert.isTrue(calc.isValid("XXVII"))
+    assert.isTrue(calc.isValid("MMMCM"))
+    assert.isTrue(calc.isValid("MMMCM"))
+    assert.isTrue(calc.isValid("MMMCMXC"))
 })
 
-test('add returns INVALID INPUT given repeated V, L or D in a numeral', ()=>{    
-    assert.equal(RomanCalculator.add("VV", "I"), "INVALID INPUT")
-    assert.equal(RomanCalculator.add("I", "LL"), "INVALID INPUT")
-    assert.equal(RomanCalculator.add("X", "DD"), "INVALID INPUT")
-    assert.equal(RomanCalculator.add("IIII", "L"), "INVALID INPUT")
-    assert.equal(RomanCalculator.add("I", "XXXX"), "INVALID INPUT")
-})
-test('add returns INVALID INPUT given I, X, C or M repeated more than 3 times in a numeral', ()=>{    
-    assert.equal(RomanCalculator.add("IIII", "L"), "INVALID INPUT")
-    assert.equal(RomanCalculator.add("I", "XXXX"), "INVALID INPUT")
-    assert.equal(RomanCalculator.add("CCCC", "MMMMMMMM"), "INVALID INPUT")
+test('isValid returns false given I, X, C or M repeated more than 3 times in a numeral', () => {    
+    const calc = new RomanCalculator();
+    assert.isFalse(calc.isValid("IIII"))
+    assert.isFalse(calc.isValid("XIIII"))
+    assert.isFalse(calc.isValid("CCCC"))
+    assert.isFalse(calc.isValid("XVIXXXX"))
 })
 
-test('add returns INVALID INPUT given a non roman numeral', ()=>{    
-    assert.equal(RomanCalculator.add("VV", "3"), "INVALID INPUT")
-    assert.equal(RomanCalculator.add("y", "LL"), "INVALID INPUT")
-    assert.equal(RomanCalculator.add("X", "d"), "INVALID INPUT")
+test('isValid returns false given a non roman numeral', () => {    
+    const calc = new RomanCalculator();
+    assert.isFalse(calc.isValid("VV"))
+    assert.isFalse(calc.isValid("y"))
+    assert.isFalse(calc.isValid("0"))
+    assert.isFalse(calc.isValid("x"))
+})
+
+test('isValid returns false given repeated V, L or D in a numeral', () => {  
+    const calc = new RomanCalculator();
+    assert.isFalse(calc.isValid("VV"))
+    assert.isFalse(calc.isValid("DD"))
+    assert.isFalse(calc.isValid("MDMDD"))
+    assert.isFalse(calc.isValid("LL"))
+    assert.isFalse(calc.isValid("XVVV"))
+})
+
+
+test('isValid returns false when a numeral\'s suffixes are not valid', () => { 
+    const calc = new RomanCalculator();    
+    assert.isFalse(calc.isValid("IC"));    
+    // assert.isFalse(calc.isValid("IIV")); 
+    assert.isFalse(calc.isValid("IIIV")); 
+    // assert.isFalse(calc.isValid("MMMCMXC")); 
+})
+
+test('toNumber gets the number value of the roman numeral', () => { 
+    const calc = new RomanCalculator();
+
+    assert.equal(calc.toNumber("I"), 1);
+    assert.equal(calc.toNumber("II"), 2);
+    assert.equal(calc.toNumber("III"), 3);
+    assert.equal(calc.toNumber("IV"), 4);
+    assert.equal(calc.toNumber("V"), 5);
+    assert.equal(calc.toNumber("VI"), 6);
+    assert.equal(calc.toNumber("VII"), 7);
+    assert.equal(calc.toNumber("VIII"), 8);
+    assert.equal(calc.toNumber("IX"), 9);
+    assert.equal(calc.toNumber("X"), 10);
+
+    assert.equal(calc.toNumber("XIII"), 13);
+    assert.equal(calc.toNumber("XIV"), 14);
+    assert.equal(calc.toNumber("XXVII"), 27);   
+    assert.equal(calc.toNumber("XXIX"), 29);   
+})
+
+test('toNumeral gets the numberal value of a digit', () => { 
+    const calc = new RomanCalculator();
+
+    assert.equal(calc.toNumeral(1), "I");
+    assert.equal(calc.toNumeral(2), "II");
+    assert.equal(calc.toNumeral(3), "III");
+    assert.equal(calc.toNumeral(4), "IV");
+    assert.equal(calc.toNumeral(5), "V");
+    assert.equal(calc.toNumeral(6), "VI");
+    assert.equal(calc.toNumeral(7), "VII");
+    assert.equal(calc.toNumeral(8), "VIII");
+    assert.equal(calc.toNumeral(9), "IX");
+    assert.equal(calc.toNumeral(10), "X");
+    assert.equal(calc.toNumeral(13), "XIII");
+    assert.equal(calc.toNumeral(14), "XIV");
+    assert.equal(calc.toNumeral(27), "XXVII");   
+    assert.equal(calc.toNumeral(29), "XXIX");   
+    assert.equal(calc.toNumeral(447), "CDXLVII");   
+    assert.equal(calc.toNumeral(543), "DXLIII");   
+    assert.equal(calc.toNumeral(1536), "MDXXXVI");  
+})
+
+test('add returns INVALID INPUT given an invlaid Numerals as a parameter', () => {  
+    const calc = new RomanCalculator();
+    assert.equal(calc.add("VV", "I"), "INVALID INPUT")
+    assert.equal(calc.add("IIII", "L"), "INVALID INPUT")
+    assert.equal(calc.add("XVIXX", "II"), "INVALID INPUT")
+})
+
+test('add sums the roman numerals', () => {   
+    const calc = new RomanCalculator();
+    assert.equal(calc.add("I", "I"), "II")
+    assert.equal(calc.add("I", "II"), "III")
+    assert.equal(calc.add("X", "X"), "XX")
+    assert.equal(calc.add("C", "C"), "CC")
+    assert.equal(calc.add("V", "I"), "VI")
+    assert.equal(calc.add("X", "II"), "XII")
+    assert.equal(calc.add("X", "VI"), "XVI")
+    assert.equal(calc.add("L", "XV"), "LXV")
+    assert.equal(calc.add("X", "XX"), "XXX")
+    
+    assert.equal(calc.add("I", "III"), "IV")
+    assert.equal(calc.add("MDVI", "XL"), "MDXLVI")
 })
--- src/romanCalculator.ts
+++ src/romanCalculator.ts
@@ -1,40 +1,126 @@
 export class RomanCalculator{
-
-    static add(numeralOne:string, numeralTwo:string):string{
-        if (!RomanCalculator.isValid(numeralOne) || !RomanCalculator.isValid(numeralTwo)){
+    add(numeralOne:string, numeralTwo:string):string  {
+        if (!this.isValid(numeralOne) || !this.isValid(numeralTwo)){
             return "INVALID INPUT"
         }
-        return numeralOne + numeralTwo;
+        const sumValue = this.toNumber(numeralOne) + this.toNumber(numeralTwo)
+        return this.toNumeral(sumValue);
     }
-    static isValid(numeral:string):boolean{
 
-        for (let index = 0; index < numeral.length; index++) {
-            if (numeralsRuleMap.has(numeral[index])) {
-                const allowedRepetitions = numeralsRuleMap.get(numeral[index])?.allowedRepetitions as number;
-                const invalidRepetition = numeral[index].repeat(allowedRepetitions + 1)
-                if (numeral.includes(invalidRepetition)) {
-                    return false;
-                }
-            }
-            else { 
-                return false;
-            }             
+    toNumber(numeral: string): number{
+        let value :number = 0;
+        for (let index = 0; index < numeral.length; index++) { 
+            const digitValue = numeralValueMap.get(numeral[index]) as number
+            const nextDigitValue = numeralValueMap.get(numeral[index+1]) as number
+            if (digitValue < nextDigitValue)
+                value -= digitValue;
+            else
+                value += digitValue;
         }
-
-        return true;
+        return value;
     }
+
+    toNumeral(digit: number): string { 
+        let romanNumeral: string="";
+        for (const numeral of Array.from(subtractionNotationMap).reverse()) {
+            let count = 0;
+            if (digit >= numeral[1]) { 
+                count = Math.trunc(digit / numeral[1]); 
+                for (let i = 0 ; i < count; i++) { 
+                    romanNumeral += numeral[0]
+                }
+                digit %= numeral[1];
+            }
+        }
+        return romanNumeral;
+    }
+
+    isValid(numeral: string): boolean{
+        const romanNumeralRegex = /^(M{0,3})(D?C{0,3}|CD|CM|)(L?X{0,3}|XL|XC)(V?I{0,3}|IV|IX)$/;
+        return romanNumeralRegex.test(numeral);
+    }
+    // isValid = (numeral: string): boolean => {
+        // for (let index = 0; index < numeral.length; index++) {
+        //     if (!numeralRuleMap.has(numeral[index])) {
+        //         console.log(`${numeral[index]} is not a roman numeral in ${numeral}`)
+        //         return false;
+        //     }              
+        //     if (hasInvalidRepetitions(numeral, numeral[index])) {
+        //         console.log(`${numeral[index]} has invalid repetitions in ${numeral}`)
+        //         return false;
+        //     } 
+        //     if (hasWrongSubtractionNotation(numeral, index)) {
+        //         console.log(`${numeral[index]} prcedes wrongly in ${numeral}`)
+        //         return false;
+        //     } 
+        // }
+        // return true;
+    // }
 }
 
-const numeralsRuleMap = new Map<string, NumeralRule>([
-    ["I", { allowedRepetitions: 3 }],
-    ["V", { allowedRepetitions: 1 }],
-    ["X", { allowedRepetitions: 3 }],
-    ["L", { allowedRepetitions: 1 }],
-    ["C", { allowedRepetitions: 3 }],
-    ["D", { allowedRepetitions: 1 }],
-    ["M", { allowedRepetitions: 3 }]
+
+
+
+/*
+const hasWrongSubtractionNotation = (numeral: string, index: number): boolean => { 
+    const numeralRule :NumeralRule  = numeralRuleMap.get(numeral[index]) as NumeralRule;
+    const allowedSuffixes = numeralRule.allowedSuffixesOfHigherValue;
+    
+    if (index + 1 <= numeral.length && allowedSuffixes!.length == 0)
+        return false;
+
+    const digitValue = numeralRuleMap.get(numeral[index])?.value as number
+    const nextDigitValue = numeralRuleMap.get(numeral[index + 1])?.value as number
+    // const nextToNextDigitValue = index + 2 < numeral.length ? numeralRuleMap.get(numeral[index + 2])?.value as number : 0
+
+    return digitValue < nextDigitValue && !allowedSuffixes!.includes(numeral[index + 1])
+        // && nextToNextDigitValue != 0 && digitValue;
+}
+
+const hasInvalidRepetitions = (numeral: string, numeralChar: string):boolean => { 
+    const allowedRepetitions = numeralRuleMap.get(numeralChar)?.allowedRepetitions as number;
+    const invalidRepetition =numeralChar.repeat(allowedRepetitions + 1)
+    return numeral.includes(invalidRepetition);
+}
+
+const numeralRuleMap = new Map<string, NumeralRule>([
+    ["I", { value: 1,    allowedRepetitions: 3,  allowedSuffixesOfHigherValue:["V", "X"]    }],
+    ["V", { value: 5,    allowedRepetitions: 1,  allowedSuffixesOfHigherValue:[]            }],
+    ["X", { value: 10,   allowedRepetitions: 3 , allowedSuffixesOfHigherValue:[ "L", "C"] }],
+    ["L", { value: 50,   allowedRepetitions: 1,  allowedSuffixesOfHigherValue:[]           }],
+    ["C", { value: 100,  allowedRepetitions: 3,  allowedSuffixesOfHigherValue:["D", "M"]  }],
+    ["D", { value: 500,  allowedRepetitions: 1,  allowedSuffixesOfHigherValue:[]          }],
+    ["M", { value: 1000, allowedRepetitions: 3,  allowedSuffixesOfHigherValue:[]         }]
 ]);
 
 type NumeralRule = {
-    allowedRepetitions:number 
+    value:number,
+    allowedRepetitions: number ,
+    allowedSuffixesOfHigherValue?: string[]
 }
+*/
+
+const numeralValueMap = new Map<string, number>([
+   ["I", 1 ],
+   ["V", 5],
+   ["X", 10],
+   ["L", 50 ],
+   ["C", 100],
+   ["D", 500],
+   ["M", 1000],
+]);
+const subtractionNotationMap = new Map<string, number>([
+    ["I", 1],
+    ["IV", 4 ],
+    ["V", 5],
+    ["IX", 9],
+    ["X", 10],
+    ["XL", 40 ],
+    ["L", 50],
+    ["XC", 90 ],
+    ["C", 100],
+    ["CD", 400 ],
+    ["D", 500],
+    ["CM", 900 ],
+    ["M", 1000],
+ ]);
